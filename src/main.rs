0f1d759132b1158a4d45fcba450e1115341f3e9c
unknown <raunak>
2022-03-11 23:33:29 +0530

    single threaded web server -reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    // bind() returns the new instance of TcpListener.
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // incoming() returns a iterator that gives us a sequence of streams
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        //println!("Connection estabblished!");
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // Capacity -> 1024 bytes.
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
